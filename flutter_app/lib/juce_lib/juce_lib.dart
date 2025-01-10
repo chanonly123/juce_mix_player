c9ce7bd773a99f1426a782892afeb79c9c9fbe4b
sky1095 <sudhanshuexcel.yadav@gmail.com>
2025-01-10 14:27:56 +0530

    Add callback support for progress, state updates, and errors in JuceMixPlayer

--- flutter_app/lib/juce_lib/juce_lib.dart
+++ flutter_app/lib/juce_lib/juce_lib.dart
@@ -6,12 +6,19 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_app/juce_lib/juce_lib_gen.dart';
 
 // Dart function typedefs
-typedef ProgressCallbackDart = void Function(Pointer<Void>, double);
-typedef StringCallbackDart = void Function(Pointer<Void>, Pointer<Utf8>);
+typedef ProgressCallbackDart = void Function(double progress);
+typedef StateUpdateCallbackDart = void Function(String state);
+typedef ErrorCallbackDart = void Function(String error);
 
 // Native function typedefs
 typedef ProgressCallbackNative = Void Function(Pointer<Void>, Float);
-typedef StringCallbackNative = Void Function(Pointer<Void>, Pointer<Utf8>);
+typedef StateUpdateCallbackNative = Void Function(Pointer<Void>, Pointer<Utf8>);
+typedef ErrorCallbackNative = Void Function(Pointer<Void>, Pointer<Utf8>);
+
+// Native callbacks storage
+final _progressCallbacks = <Pointer<Void>, ProgressCallbackDart>{};
+final _stateCallbacks = <Pointer<Void>, StateUpdateCallbackDart>{};
+final _errorCallbacks = <Pointer<Void>, ErrorCallbackDart>{};
 
 class JuceLib extends JuceLibGen {
   JuceLib() : super(defaultTargetPlatform == TargetPlatform.iOS ? DynamicLibrary.process() : DynamicLibrary.open("libjuce_lib.so"));
@@ -23,6 +30,33 @@ class JuceMixPlayer {
 
   JuceMixPlayer() {
     _ptr = _juceLib.JuceMixPlayer_init();
+
+    // Set up callbacks
+    _juceLib.JuceMixPlayer_onProgress(_ptr, Pointer.fromFunction<ProgressCallbackNative>(_onProgressCallback));
+    _juceLib.JuceMixPlayer_onStateUpdate(_ptr, Pointer.fromFunction<StateUpdateCallbackNative>(_onStateUpdateCallback));
+  }
+
+  // Static callback handlers
+  static void _onProgressCallback(Pointer<Void> ptr, double progress) {
+    _progressCallbacks[ptr]?.call(progress);
+  }
+
+  static void _onStateUpdateCallback(Pointer<Void> ptr, Pointer<Utf8> state) {
+    final stateStr = state.toDartString();
+    _stateCallbacks[ptr]?.call(stateStr);
+  }
+
+  // Public callback setters
+  void setProgressHandler(ProgressCallbackDart callback) {
+    _progressCallbacks[_ptr] = callback;
+  }
+
+  void setStateUpdateHandler(StateUpdateCallbackDart callback) {
+    _stateCallbacks[_ptr] = callback;
+  }
+
+  void setErrorHandler(ErrorCallbackDart callback) {
+    _errorCallbacks[_ptr] = callback;
   }
 
   void setFile(String path) {
@@ -59,6 +93,11 @@ class JuceMixPlayer {
   }
 
   void dispose() {
+    // Clear callbacks
+    _progressCallbacks.remove(_ptr);
+    _stateCallbacks.remove(_ptr);
+    _errorCallbacks.remove(_ptr);
+
     _juceLib.JuceMixPlayer_deinit(_ptr);
   }
 }
